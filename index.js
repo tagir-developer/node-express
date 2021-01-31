8694ae89a57a673fda67c0513b9dbfb60604887e
Tagir Faskhutdinov (home PC) <tagirdjan@gmail.com>
2021-01-31 20:41:05 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const path = require('path')
+const csrf = require('csurf')
+const flash = require('connect-flash')
+const mongoose = require('mongoose')
+const Handlebars = require('handlebars')
+const exphbs = require('express-handlebars')
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
+const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
+const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
+const addRoutes = require('./routes/add')
+const ordersRoutes = require('./routes/orders')
+const coursesRoutes = require('./routes/courses')
+const authRoutes = require('./routes/auth')
+const varMiddleware = require('./middleware/variables')
+const userMiddleware = require('./middleware/user')
+
+const MONGODB_URI = `mongodb+srv://tagir:1aQrW1P9AzfcVVUn@cluster0.wuhvj.mongodb.net/shop`
+const PORT = process.env.PORT || 3000
+
+const app = express()
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs',
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
+})
+const store = new MongoStore({
+  collection: 'sessions',
+  uri: MONGODB_URI
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
+app.use(session({
+  secret: 'some secret value',
+  resave: false,
+  saveUninitialized: false,
+  store
+}))
+app.use(csrf())
+app.use(flash())
+app.use(varMiddleware)
+app.use(userMiddleware)
+
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
+app.use('/card', cardRoutes)
+app.use('/orders', ordersRoutes)
+app.use('/auth', authRoutes)
+
+async function start() {
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useFindAndModify: false
+    })
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
+
+
