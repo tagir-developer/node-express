537ee1077b86c762bc1c5594081e30ce3b01ec77
Tagir Faskhutdinov (home PC) <tagirdjan@gmail.com>
2021-01-31 21:19:51 +0300

    sengrid-api-key

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const coursesRoutes = require('./routes/courses')
 const authRoutes = require('./routes/auth')
 const varMiddleware = require('./middleware/variables')
 const userMiddleware = require('./middleware/user')
+const keys = require('./keys')
 
-const MONGODB_URI = `mongodb+srv://tagir:1aQrW1P9AzfcVVUn@cluster0.wuhvj.mongodb.net/shop`
 const PORT = process.env.PORT || 3000
 
 const app = express()
@@ -28,7 +28,7 @@ const hbs = exphbs.create({
 })
 const store = new MongoStore({
   collection: 'sessions',
-  uri: MONGODB_URI
+  uri: keys.MONGODB_URI
 })
 
 app.engine('hbs', hbs.engine)
@@ -38,7 +38,7 @@ app.set('views', 'views')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 app.use(session({
-  secret: 'some secret value',
+  secret: keys.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   store
@@ -57,7 +57,7 @@ app.use('/auth', authRoutes)
 
 async function start() {
   try {
-    await mongoose.connect(MONGODB_URI, {
+    await mongoose.connect(keys.MONGODB_URI, {
       useNewUrlParser: true,
       useFindAndModify: false
     })
---
+++ keys/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+	MONGODB_URI: `mongodb+srv://tagir:1aQrW1P9AzfcVVUn@cluster0.wuhvj.mongodb.net/shop`,
+	SESSION_SECRET: 'some secret value',
+	SENDGRID_API_KEY: 'SG.Vz-SdxztTCGUpjWlH5aSDw.NGGYvyKmvzW7HxlxG16zdNJ7DUbPjVdwowSKxgwvkYw'
+}
