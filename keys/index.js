c99ee1eb4ecfbd2703e0fc597e9b20822d2e1d6e
Tagir Faskhutdinov (work) <tagirdjan@gmail.com>
2021-02-01 19:49:33 +0300

    sendgrid sends email

--- keys/index.js
+++ keys/index.js
@@ -1,5 +1,7 @@
 module.exports = {
 	MONGODB_URI: `mongodb+srv://tagir:1aQrW1P9AzfcVVUn@cluster0.wuhvj.mongodb.net/shop`,
 	SESSION_SECRET: 'some secret value',
-	SENDGRID_API_KEY: 'SG.Vz-SdxztTCGUpjWlH5aSDw.NGGYvyKmvzW7HxlxG16zdNJ7DUbPjVdwowSKxgwvkYw'
+	SENDGRID_API_KEY: 'SG.Vz-SdxztTCGUpjWlH5aSDw.NGGYvyKmvzW7HxlxG16zdNJ7DUbPjVdwowSKxgwvkYw',
+	EMAIL_FROM: 'orlov.marsel@ya.ru',
+	BASE_URL: 'http://localhost:3000/'
 }
